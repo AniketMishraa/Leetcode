fe420656e06cd29d7a50381cf1100bfdde34e159
Aniket Mishra <aaruto@gmail.com>
2022-12-29 18:37:36 -0800

    Update averagesalary.py

--- averagesalary.py
+++ averagesalary.py
@@ -2,13 +2,8 @@
 
 class Solution:
     def average(self, salary: List[int]) -> float:
-        a = 0
-        salary.sort()
-        salary.pop(-1)
+        salary = sorted(salary)
+        salary.pop()
         salary.pop(0)
-        for i in salary:
-            a += i
-        a = a/len(salary)
-        return a
-            
+        return sum(salary) / len(salary)
         
