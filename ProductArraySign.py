5c5da3be644f68e59fd0420e657ff50d2d384164
Aniket Mishra <aaruto@gmail.com>
2023-01-02 11:23:36 -0800

    Add files via upload

---
+++ ProductArraySign.py
@@ -0,0 +1,11 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        temp = 1
+        for i in nums:
+            temp *= i
+        if temp > 0:
+            return 1
+        elif temp < 0:
+            return -1
+        else:
+            return 0
